42dcd116f17ab76de6bc8acf5088c8c2ac34d951
sumayamusa <sumayamusa142@gmail.com>
2025-10-23 21:39:08 +0000

    inital commit

---
+++ credentials.php
@@ -0,0 +1,8 @@
+<?php
+//REMEMBER TO POPULATE THE DATA HERE
+
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
+?>
